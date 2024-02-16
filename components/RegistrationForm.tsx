f57e2b3459b17bbb73962aadbf7fa3b21789b6d5
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-02-16 09:18:29 -0300

    style: mudança na lable do formulário de registro

--- components/RegistrationForm.tsx
+++ components/RegistrationForm.tsx
@@ -96,7 +96,7 @@ const RegistrationForm = () => {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+              Senha
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
